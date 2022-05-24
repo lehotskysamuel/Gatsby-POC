364e772b5767784edfe90e184ad8553023c722d0
Samuel Lehotsky <lehotsky@atlas.sk>
2022-05-24 21:42:09 +0200

    media descriptions + media fragments for video

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import "./image.scss";
 
-export const Image = ({ src, alt }) => {
-  return <img className="image" src={src} alt={alt} />;
+export const Image = ({ src, desc }) => {
+  return <img className="image" src={src} alt={desc} />;
 };
 
-export const ImageInline = ({ src, alt }) => {
-  return <img className="image-inline" src={src} alt={alt} />;
+export const ImageInline = ({ src, desc }) => {
+  return <img className="image-inline" src={src} alt={desc} />;
 };
--- src/components/video.jsx
+++ src/components/video.jsx
@@ -3,10 +3,18 @@ import React from "react";
 
 import "./video.scss";
 
-export const Video = ({ src, type = "video/mp4" }) => {
+export const Video = ({ src, start, end, desc, type = "video/mp4" }) => {
   return (
     <video className="video" controls={true} preload="auto" playsInline={true}>
-      <source src={src} type={type} />
+      <source
+        // Start and End props are called Media Fragments, Temporal Dimension fragment specifically
+        // See more on what content they might contain here: https://www.w3.org/TR/media-frags/#naming-time
+        // The spec is still just a recommendation and doesn't work ideally (still allows me to play the rest of the file, whereas I would like to omit it)
+        src={`${src}${start || end ? "#t=" : ""}${start || ""}${
+          end ? `,${end}` : ""
+        }`}
+        type={type}
+      />
     </video>
   );
 };
